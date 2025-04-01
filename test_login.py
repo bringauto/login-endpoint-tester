3e37c2dc14c1b10c62c99256a63ec6cea121c89d
MarioIvancik <mario.ivancik@bringauto.com>
2025-04-01 12:59:24 +0200

    retry login multiple times

--- test_login.py
+++ test_login.py
@@ -4,6 +4,7 @@ from playwright.sync_api import sync_playwright, Playwright # type: ignore
 
 CONFIG_PATH = "config/config.json"
 PAGE_LOAD_TIMEOUT = 10000
+RETRY_ATTEMPTS = 3
 
 
 class Config:
@@ -16,22 +17,28 @@ class Config:
 
 
 def run(playwright: Playwright, config: Config):
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context()
-    page = context.new_page()
-    try:
-        page.goto(config.login_url)
-        page.wait_for_selector("input[name='username']", timeout=PAGE_LOAD_TIMEOUT)
-        page.wait_for_selector("input[name='password']", timeout=PAGE_LOAD_TIMEOUT)
-        page.fill("input[name='username']", config.username)
-        page.fill("input[name='password']", config.password)
-        with page.expect_navigation() as response_info:
-            page.click("button[id='kc-login']")
-            page.wait_for_url(config.redirect_url, timeout=PAGE_LOAD_TIMEOUT)
-    except Exception as e:
-        print("[ERROR] Login failed! Timeout!")
-        print(e)
-        exit(1)
+    for i in range(RETRY_ATTEMPTS):
+        browser = playwright.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+        try:
+            page.goto(config.login_url)
+            page.wait_for_selector("input[name='username']", timeout=PAGE_LOAD_TIMEOUT)
+            page.wait_for_selector("input[name='password']", timeout=PAGE_LOAD_TIMEOUT)
+            page.fill("input[name='username']", config.username)
+            page.fill("input[name='password']", config.password)
+            with page.expect_navigation() as response_info:
+                page.click("button[id='kc-login']")
+                page.wait_for_url(config.redirect_url, timeout=PAGE_LOAD_TIMEOUT)
+                break
+        except Exception as e:
+            print("[WARNING] Login failed! Timeout!")
+            print(e)
+            if i == RETRY_ATTEMPTS - 1:
+                print("[ERROR] Login failed!")
+                exit(1)
+            browser.close()
+
     response = response_info.value
     if response.status != 200:
         print("[ERROR] Login failed!")
